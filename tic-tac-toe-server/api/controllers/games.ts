2ad07cf891add773a71d04edb13b140418de0d3b
Curtis Schlak <curtis.schlak@theironyard.com>
2017-03-07 04:22:21 -0600

    Create interface for SwaggerRequest.

--- tic-tac-toe-server/api/controllers/games.ts
+++ tic-tac-toe-server/api/controllers/games.ts
@@ -1,21 +1,23 @@
 import * as express from 'express';
+import { SwaggerRequest } from './swaggerInterop';
 
 export function list(req: express.Request, res: express.Response) : void {
 
 };
 
-export function create(req: express.Request, res: express.Response) : void {
-
+export function create(req: SwaggerRequest, res: express.Response) : void {
+  let command = req.swagger.params.gameRequest.value;
 };
 
-export function details(req: express.Request, res: express.Response) : void {
-
+export function details(req: SwaggerRequest, res: express.Response) : void {
+  let gameId = req.swagger.params.id.value;
 };
 
-export function update(req: express.Request, res: express.Response) : void {
-
+export function update(req: SwaggerRequest, res: express.Response) : void {
+  let gameId = req.swagger.params.id.value;
+  let command = req.swagger.params.move.value;
 };
 
-export function remove(req: express.Request, res: express.Response) : void {
-
+export function remove(req: SwaggerRequest, res: express.Response) : void {
+  let gameId = req.swagger.params.id.value;
 };
---
+++ tic-tac-toe-server/api/controllers/swaggerInterop.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+
+export interface ParamsHolder {
+  params: any
+}
+
+export interface SwaggerRequest extends express.Request {
+  swagger: ParamsHolder;
+}
