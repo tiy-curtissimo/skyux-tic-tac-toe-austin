14666bbe26f8e57e4bb23e013d1fb243f04164d8
Curtis Schlak <curtis.schlak@theironyard.com>
2017-03-08 08:26:46 -0600

    Add HTTPS for server.

--- tic-tac-toe-server/app.js
+++ tic-tac-toe-server/app.js
@@ -1,8 +1,16 @@
 'use strict';
 
+var fs = require('fs');
+var https = require('https');
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
+
+var options = {
+  key: fs.readFileSync('./config/server.key'),
+  cert: fs.readFileSync('./config/server.crt'),
+  ca: fs.readFileSync('./config/ca.crt')
+};
 
 var config = {
   appRoot: __dirname // required config
@@ -15,7 +23,9 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
-  app.listen(port);
+  https.createServer(options, app).listen(port, function () {
+    console.log('Server started @ %s', port);
+  });
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
